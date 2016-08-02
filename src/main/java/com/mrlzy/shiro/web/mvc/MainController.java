c9f2ec71bec1b516d3bcbd1670997545ee67f1a0
mrlzy <285204998@qq.com>
2016-08-02 17:22:32 +0800

    first commit

--- src/main/java/com/mrlzy/shiro/web/mvc/MainController.java
+++ src/main/java/com/mrlzy/shiro/web/mvc/MainController.java
@@ -1,21 +1,15 @@
 package com.mrlzy.shiro.web.mvc;
 
-import com.mrlzy.app.Constants;
-import com.mrlzy.shiro.dao.local.jpa.MenuJpa;
 import com.mrlzy.shiro.dao.local.mapper.MenuMapper;
-import com.mrlzy.shiro.entity.Menu;
 import com.mrlzy.shiro.entity.User;
 import com.mrlzy.shiro.plugin.dto.CaseIgnoreDto;
-import com.mrlzy.shiro.plugin.mvc.UserSelf;
 import com.mrlzy.shiro.session.ShiroSessionUtils;
-import com.mrlzy.shiro.tool.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
@@ -28,9 +22,8 @@ public class MainController {
 
     @RequestMapping("/getMenu")
     @ResponseBody
-    public List<CaseIgnoreDto> getMenu(HttpServletRequest request, @RequestParam("menu_id") String menu_id ) {
+    public List<CaseIgnoreDto> getMenu(@RequestParam("menu_id") String menu_id ) {
        User user=ShiroSessionUtils.getCurrUser();
-        System.out.println(JsonUtil.obj2Json( menuMapper.getListMenuByParentId(user.getRole().getRole_id(),menu_id)));
         return menuMapper.getListMenuByParentId(user.getRole().getRole_id(),menu_id);
     }
 
